6e3a7b50e023980236688c2af879f9ae601f9f4b
EC2 Default User <ec2-user@ip-172-31-1-190.us-east-2.compute.internal>
2022-06-17 20:48:43 +0000

    first commint on develop

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+@appx.route('/f')
+def first():
+     return 'first example'
 
 if __name__ == '__main__':
     app.run()
